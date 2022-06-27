29a9afdc2535b1324c4f58bc3e066724f8461b53
FukudaTaiga <taiga.fukuda@hashport.io>
2022-06-27 16:33:14 +0900

    add dummy impl

--- src/median.sol
+++ src/median.sol
@@ -173,3 +173,14 @@ contract Median is LibNote {
         }
     }
 }
+
+contract MedianJPXJPY is Median {
+    bytes32 public constant wat = "jpxjpy";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
